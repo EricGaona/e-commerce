c2cb588a95f387cffa8ab70c10abde8c45f54c54
ericgaona <ericgaona0413@gmail.com>
2020-04-25 13:57:53 +0000

    working ecommerce site

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_uvxVwegGmV52rXybCJiriBz2009duXyALW")
+os.environ.setdefault("STRIPE_SECRET", "sk_test_ITJAPfug3t0By64l1YlVwo1a00XYx7ZfPu")
+os.environ.setdefault("DATABASE_URL", "postgres://oxgipqvirvjfsb:00d9d662bbcb9a5ae347935fb26195d0f684dbd1d7e21de5441d0e98c8a00634@ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:5432/dq5hh3gmfilpq")
+os.environ.setdefault("SECRET_KEY", "^yha9$$nvbsgmiiacxxs2e=@120u(0dlgbb#iq$)(3#keoxyk2")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIA3AHVE4MIHGOANCNY")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
